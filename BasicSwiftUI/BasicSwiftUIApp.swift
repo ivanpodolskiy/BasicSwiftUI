d1eaedb94ae544de68ff69c3ab40d219973f97e8
ivanpodolskiy <stren.25@mail.ru>
2022-05-19 14:48:15 +0300

    Initial Commit

---
+++ BasicSwiftUI/BasicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicSwiftUIApp.swift
+//  BasicSwiftUI
+//
+//  Created by user on 19.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct BasicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
